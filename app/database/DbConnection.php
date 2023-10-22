27e068bd2bc9961ebd015c588a9eee5ef54acb40
CvijicZ <115740663+CvijicZ@users.noreply.github.com>
2023-10-22 13:38:35 +0200

    Created database connection class

---
+++ app/database/DbConnection.php
@@ -0,0 +1,36 @@
+<?php
+
+class DbConnection
+{
+    private static $instance;
+    private $pdo;
+
+    private function __construct()
+    {
+        try {           
+            require_once dirname(__DIR__,2) . "/vendor/autoload.php";
+
+            $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,2));
+            $dotenv->safeLoad();
+
+            $this->pdo = new PDO("mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']};charset=utf8mb4", $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+        } 
+        catch (PDOException $e) {
+            header("location: /wheelsforsale/index.php?error=dbError");
+            exit(); 
+        }
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+}
